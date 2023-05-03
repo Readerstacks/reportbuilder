60244b1c37a4cde51ca3b1605fe00c7ec1d735f5
aman <ajain@abnosoftwares.co.ke>
2023-05-03 13:00:34 +0530

    Add existing project files to Git

--- src/migrations/2023_04_21_132240_create_report_builder_questions.php
+++ src/migrations/2023_04_21_132240_create_report_builder_questions.php
@@ -16,13 +16,13 @@ class CreateReportBuilderQuestions extends Migration
         Schema::create('report_builder_questions', function (Blueprint $table) {
             $table->increments('id');
             $table->string("title");
-            $table->text("sql_query");
-            $table->text("layout");
-            $table->text("filters");
-            $table->string("visibility");
-            $table->string("token");
-            $table->string("uuid_token");
-            $table->string("connection");
+            $table->text("sql_query")->nullable();
+            $table->text("layout")->nullable();
+            $table->text("filters")->nullable();
+            $table->string("visibility")->nullable();
+            $table->string("token")->nullable();
+            $table->string("uuid_token")->nullable();
+            $table->string("connection")->nullable();
             $table->enum("status",['1','0']);
             $table->timestamps();
         });
