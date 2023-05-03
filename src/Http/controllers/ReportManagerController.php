05077db19c0636c154963855e666d55d1587e090
aman <ajain@abnosoftwares.co.ke>
2023-05-03 13:21:55 +0530

    Report Tool

--- src/Http/controllers/ReportManagerController.php
+++ src/Http/controllers/ReportManagerController.php
@@ -18,7 +18,7 @@ class ReportManagerController extends Controller
         
     }
     function builder(){
-        return (new \Aman5537jains\ReportBuilder\ReportGenerator())->render();
+        return (new \Aman5537jains\ReportBuilder\ReportGenerator())->render(); 
    }
     function showReport($id){
         $report=  ReportBuilderQuestion::where("uuid_token",$id)->first();
