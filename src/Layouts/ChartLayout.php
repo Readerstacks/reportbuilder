5b26ebcb9b7d43599234c6f7d7b35ebcd92182ef
aman <ajain@abnosoftwares.co.ke>
2023-07-21 16:15:32 +0530

    changes

--- src/Layouts/ChartLayout.php
+++ src/Layouts/ChartLayout.php
@@ -31,60 +31,64 @@ class ChartLayout extends BaseLayout
         $columns =  explode(",",$this->layoutSettings['data_column']); // Y AXIS
         $rows =  explode(",",$this->layoutSettings['label_column']); //X AXIS
         $xAxis = [];
-     
+        try{
             foreach ($this->reportBuilder->rows as $row) {
                 if(!isset($xAxis[$row->row->{$rows[0]}])){
                     $xAxis[$row->row->{$rows[0]}]=[];
                 }
                 $col='data';
                 if(isset($rows[1])){
-                   
+                    
                     $col=$row->row->{$rows[1]};
                 }
                 $xAxis[$row->row->{$rows[0]}][$col]  = $row->row->{$columns[0]};
                 $dataNames[$col]=$col;
             }
 
-         foreach($xAxis as $x=>$value){
-            $allRows[]=$x;
-            foreach($dataNames as $dataName){
-                if(!isset($value[$dataName])){
-                    $xAxis[$x][$dataName]=0 ;
+            foreach($xAxis as $x=>$value){
+                $allRows[]=$x;
+                foreach($dataNames as $dataName){
+                    if(!isset($value[$dataName])){
+                        $xAxis[$x][$dataName]=0 ;
+                    }
+                }
+                
+            }   
+            foreach($dataNames  as $k=>$val){
+                $values=[];
+                foreach($xAxis as $v){
+                $values[]= $v[$k];
+                }
+                $allColumns[]=["label"=>$k,"data"=>$values];
+            }
+            
+            
+        
+        $allRows =array_keys($xAxis);
+
+            foreach ($this->reportBuilder->rows as $row) {
+                if (isset($row->row->{$this->layoutSettings['colors_column']})) {
+                    $colors_column[] = $row->row->{$this->layoutSettings['colors_column']};
                 }
             }
             
-         }   
-         foreach($dataNames  as $k=>$val){
-            $values=[];
-            foreach($xAxis as $v){
-               $values[]= $v[$k];
-            }
-            $allColumns[]=["label"=>$k,"data"=>$values];
-         }
-        
-         
-      
-       $allRows =array_keys($xAxis);
-
-        foreach ($this->reportBuilder->rows as $row) {
-            if (isset($row->row->{$this->layoutSettings['colors_column']})) {
-                $colors_column[] = $row->row->{$this->layoutSettings['colors_column']};
-            }
-        }
-        
-        // dd($allRows,$allColumns);
-        $colors=["red","green","yellow","blue","orange","black","#dc3545",'#18833f','#927a0e'];
-        if(count($labels)>0 && count($colors_column)<=0){
-            if(!empty($this->layoutSettings['colors_column']))
-            {
-                $colors_column = explode(",",$this->layoutSettings['colors_column']);
-            }
-            else{
-                $count=count($labels);
-                for($i=0;$i<$count;$i++){
-                    $colors_column[] =  $colors[$i];
+            // dd($allRows,$allColumns);
+            $colors=["red","green","yellow","blue","orange","black","#dc3545",'#18833f','#927a0e'];
+            if(count($labels)>0 && count($colors_column)<=0){
+                if(!empty($this->layoutSettings['colors_column']))
+                {
+                    $colors_column = explode(",",$this->layoutSettings['colors_column']);
+                }
+                else{
+                    $count=count($labels);
+                    for($i=0;$i<$count;$i++){
+                        $colors_column[] =  $colors[$i];
+                    }
                 }
             }
+        }
+        catch(\Exception $err){
+
         }
         $labels = ($allRows);
         $data_column = ($allColumns);
